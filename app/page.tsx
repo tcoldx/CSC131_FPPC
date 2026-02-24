a8623a510a747285c6d76b728629ee31db724773
Tredis Ingram <t.tredis@gmail.com>
2026-02-23 20:52:02 -0800

    basic-adds

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,44 @@
-import Image from "next/image";
 
+'use client';
+import React, {useState} from 'react';
 export default function Home() {
+  const [searchItem, setSearchitem] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+
+  const dummyData = [
+    { id: 1, name: 'Jeff Dawson', email: 'jeff.dawson@example.com' },
+    { id: 2, name: 'lee erickson', email: 'jeff.dawson@example.com' },
+    { id: 3, name: 'Lawrence Hayes', email: 'jeff.dawson@example.com' },
+  ];
+
+  const handleChange = (e: any) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filteredData = dummyData.find((item) => item.name.toLowerCase() === searchTerm);
+    setSearchitem(filteredData ? filteredData.name : '');
+  }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const filteredDummyData = dummyData.find((item) => item.name.toLowerCase() === searchItem.toLowerCase());
+    setSearchResult(filteredDummyData ? filteredDummyData.name : 'No results found');
+    console.log(searchResult);
+  }
+  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      <div className="flex flex-col items-center gap-4 gap-8">
+        {/* content container for dashboard and input */}
+        <div className="flex flex-row items-center gap-4">
+          <div className="flex items-center justify-center w-64 h-65 border border-gray-300">
+            <h1 className="text-gray-500">{searchResult || "no name found!"}</h1>
+          </div>
+          <div className="flex items-center justify-center w-64 h-65 border border-gray-300"></div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        {/* search input */}
+        <div className="flex flex-row justify-center items-center gap-4">
+      <input onChange={(e) => handleChange(e)} type="text" placeholder="Search" className="rounded-md border-2 border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none dark:bg-gray-800 dark:border-gray-600 dark:focus:border-blue-400" />
+      <button className="bg-green-300 border border-gray-300 px-4 py-2 rounded-md" onClick={(e) => handleSubmit(e)}>Submit</button>
+      </div>
+      </div>
     </div>
   );
 }
