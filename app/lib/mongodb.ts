2bf2b2804a51bb470fadfb8737086ba79614a07f
Tredis Ingram <t.tredis@gmail.com>
2026-04-23 11:56:22 -0700

    vercell

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,23 +1,20 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
 declare global {
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-function getClientPromise(): Promise<MongoClient> {
+export function getMongoClient(): Promise<MongoClient> {
+  const uri = process.env.MONGODB_URI;
+
   if (!uri) {
     throw new Error("Missing MONGODB_URI environment variable");
   }
 
   if (process.env.NODE_ENV === "development") {
     if (!global._mongoClientPromise) {
-      client = new MongoClient(uri);
+      const client = new MongoClient(uri);
       global._mongoClientPromise = client.connect();
       console.log("Created new MongoDB client promise");
     }
@@ -25,10 +22,6 @@ function getClientPromise(): Promise<MongoClient> {
     return global._mongoClientPromise;
   }
 
-  client = new MongoClient(uri);
+  const client = new MongoClient(uri);
   return client.connect();
 }
-
-clientPromise = getClientPromise();
-
-export default clientPromise;
